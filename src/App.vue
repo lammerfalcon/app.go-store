a8385c66c1dfb1ed810670373adeb9bfff4e1013
rodiongavrilov <rodiongavrilov@gmail.com>
2024-03-16 22:09:47 +0200

    fix(tma): close app

--- src/App.vue
+++ src/App.vue
@@ -34,8 +34,6 @@ async function handleCreateOrder() {
     await router.push({ name: 'payment' })
   }
   else {
-    window.Telegram.WebApp.close()
-
     // showConfirm('Вы подтверждаете перевод?', async (ok) => {
     //   if (ok) {
     try {
@@ -47,7 +45,7 @@ async function handleCreateOrder() {
       }
 
       await createOrder(payload)
-      await fetchAndSetProducts()
+      window.Telegram.WebApp.close()
     }
     catch (error) {
       console.error('Error creating order:', error)

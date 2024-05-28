9a7222fac8029cb494fa259e08e04ee4ad811117
rodiongavrilov <rodion.gavrilov@adskill.io>
2024-05-28 14:50:35 +0300

    add startapp params

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,18 @@
 import { ofetch } from 'ofetch'
 import type { FetchOptions } from 'ofetch'
 import defu from 'defu'
+import { useWebApp } from 'vue-tg'
+
+const { initDataUnsafe: { start_param } } = useWebApp()
 
 export function useApi<T>(url: string, options: FetchOptions = {}) {
+  const headers: HeadersInit = {
+    'Content-Type': 'application/json',
+    'X-Start-Param': start_param || '',
+  }
   const fetchOptions: FetchOptions = {
     baseURL: import.meta.env.VITE_BASE_API_URL,
-    headers: {
-      'Content-Type': 'application/json',
-    },
+    headers,
   }
   const mergedOptions = defu(options, fetchOptions)
   return ofetch<T>(url, mergedOptions)

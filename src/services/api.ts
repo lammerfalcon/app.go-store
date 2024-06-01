d4e136c9753599d2e21be661b0c01f800ecc7a9d
rodiongavrilov <rodion.gavrilov@adskill.io>
2024-06-01 13:26:24 +0300

    dev env

--- src/services/api.ts
+++ src/services/api.ts
@@ -8,7 +8,7 @@ const { initDataUnsafe: { start_param } } = useWebApp()
 export function useApi<T>(url: string, options: FetchOptions = {}) {
   const headers: HeadersInit = {
     'Content-Type': 'application/json',
-    'X-Start-Param': start_param || import.meta.env.DEV ? 'lammerfalcon' : '',
+    'X-Start-Param': start_param || '',
   }
   const fetchOptions: FetchOptions = {
     baseURL: import.meta.env.VITE_BASE_API_URL,
